a1dfae8e47925c2f4e5f2d4be8aed9e3cbd6cdb1
Joan Currie <jecurrie8@gmail.com>
2019-08-25 09:00:23 -0400

    physics

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,8 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Semiconductor Group"
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Semiconductor Group"
   end
 
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | Semiconductor Group"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Semiconductor Group"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Semiconductor Group"
+  end
 end
