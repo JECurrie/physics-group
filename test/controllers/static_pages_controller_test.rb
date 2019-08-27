5da04d114cdc68d6bc963c4b61d6cb21c3e35867
Joan Currie <jecurrie8@gmail.com>
2019-08-27 17:13:05 -0400

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,26 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
-    assert_select "title", "Semiconductor Group"
+    assert_select "title", "Physics Group"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Help | Semiconductor Group"
+    assert_select "title", "Help | Physics Group"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About | Semiconductor Group"
+    assert_select "title", "About | Physics Group"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
-    assert_select "title", "Contact | Semiconductor Group"
+    assert_select "title", "Contact | Physics Group"
   end
 end
