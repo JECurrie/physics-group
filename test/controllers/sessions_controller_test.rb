455cb3a61c5c3125ca0bb7af67b4eb8a29fe0ad8
Joan Currie <jecurrie8@gmail.com>
2019-09-09 15:43:38 -0400

    Implement advanced login

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-
+#=begin
   test "should get new" do
     get login_path
     assert_response :success
   end
+#=end  
 end
